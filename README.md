3d45e661b3716370436569ee2e951dcf3d84e0f9
ibuetler <ivan.buetler@hacking-lab.com>
2024-10-22 20:44:11 +0200

    update

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 # alpine-nginx-nodejs-websocketd-hl
 Alpine Docker Image-HL with NGINX, NodeJS, WebSocketd
 
-* alpine linux
-* nginx reverse proxy
-* nodejs express ui
-* websocketd handler for scripts
-* scripts being executed from the GUI
-
+# Alias
+* azure-log-viewer
 
 # idocker-prod
 * installed on /opt/applic/alpine-nginx-nodejs-websocketd-hl
--- build-multi-arch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-nginx-nodejs-websocketd-hl:latest . --push
-docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-nginx-nodejs-websocketd-hl:$1  . --push
-docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-nginx-nodejs-websocketd-hl:$1.0 . --push
